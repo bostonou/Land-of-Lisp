96417cbb8f12eaf80da6a7f06a3551a918fccf2c
Jonathan Boston <boston@chgr.mc.vanderbilt.edu>
2012-05-02 16:46:00 -0500

    #ash function

--- src/land_of_lisp/core.clj
+++ src/land_of_lisp/core.clj
@@ -1,1 +1,7 @@
 (ns land-of-lisp.core)
+
+(defn ash
+  [integer num-bits]
+  (if (> num-bits 0)
+    (bit-shift-left integer num-bits)
+    (bit-shift-right integer (* num-bits -1))))
--- test/land_of_lisp/test/core.clj
+++ test/land_of_lisp/test/core.clj
@@ -2,5 +2,9 @@
   (:use [land-of-lisp.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(testing "Shift operator"
+  (is (= (ash 11 1) 22))
+  (is (= (ash 11 -1) 5))
+  (is (= (ash 16 1) 32))
+  (is (= (ash 16 0) 16))
+  (is (= (ash 16 -1) 8)))
