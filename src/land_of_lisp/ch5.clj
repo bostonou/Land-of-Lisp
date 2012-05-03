2709ff08a2d7f599dbc71872596acb747efa531d
Jonathan Boston <boston@chgr.mc.vanderbilt.edu>
2012-05-03 12:00:44 -0500

    ch5/edges map

--- src/land_of_lisp/ch5.clj
+++ src/land_of_lisp/ch5.clj
@@ -5,6 +5,10 @@
             :garden '(you are in a beautiful garden. there is a well in front of you.)
             :attic '(you are in the attic. there is a giant welding torch in the corner.)})
 
+(def edges {:living-room ['(garden west door) '(attic upstairs ladder)]
+            :garden '(living-room east door)
+            :attic '(living-room downstairs ladder)})
+
 (defn describe-location
   [location descriptions]
   (descriptions location))
