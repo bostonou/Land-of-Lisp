cad6d5f932a31fd18b4fb2052f0c6e04c3e111c1
Jonathan Boston <boston@chgr.mc.vanderbilt.edu>
2012-05-03 11:50:46 -0500

    ch5/nodes map

--- src/land_of_lisp/ch5.clj
+++ src/land_of_lisp/ch5.clj
@@ -1,2 +1,6 @@
 (ns land-of-lisp.ch5
   [:require land-of-lisp.core])
+
+(def nodes {:living-room '(you are in the living room. a wizard is snoring loudly on the couch.)
+            :garden '(you are in a beautiful garden. there is a well in front of you.)
+            :attic '(you are in the attic. there is a giant welding torch in the corner.)})
