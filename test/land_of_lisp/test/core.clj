abadd4174b9cc6a9036d98dcc7e16dd2e9bb6539
Jonathan Boston <boston@chgr.mc.vanderbilt.edu>
2012-05-03 08:46:32 -0500

    Define as a test.

--- test/land_of_lisp/test/core.clj
+++ test/land_of_lisp/test/core.clj
@@ -2,9 +2,10 @@
   (:use [land-of-lisp.core])
   (:use [clojure.test]))
 
-(testing "Shift operator"
-  (is (= (ash 11 1) 22))
-  (is (= (ash 11 -1) 5))
-  (is (= (ash 16 1) 32))
-  (is (= (ash 16 0) 16))
-  (is (= (ash 16 -1) 8)))
+(deftest test-ash
+  (testing "Shift operator"
+    (is (= (ash 11 1) 22))
+    (is (= (ash 11 -1) 5))
+    (is (= (ash 16 1) 32))
+    (is (= (ash 16 0) 16))
+    (is (= (ash 16 -1) 8))))
