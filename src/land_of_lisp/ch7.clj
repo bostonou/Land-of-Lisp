8dd619cba32da5f1e40f4892289c1f379bc487fd
Jonathan Boston <boston@chgr.mc.vanderbilt.edu>
2012-05-05 16:21:04 -0500

    Allow generating node labels

--- src/land_of_lisp/ch7.clj
+++ src/land_of_lisp/ch7.clj
@@ -1,6 +1,15 @@
 (ns land-of-lisp.ch7
   (:require [land-of-lisp.core]))
 
+(def max-label-len 30)
+
 (defn dot-name
   [raw-name]
   (clojure.string/replace (str raw-name) #"[^\w]" "_"))
+
+(defn dot-label
+  [exp]
+  (let [s (str exp)]
+    (if (> (count s) max-label-len)
+      (str (subs s 0 max-label-len) "...")
+      s)))
--- test/land_of_lisp/test/ch7.clj
+++ test/land_of_lisp/test/ch7.clj
@@ -2,8 +2,14 @@
   (:use [land-of-lisp.ch7])
   (:use [clojure.test]))
 
-(testing "Node IDs"
+(testing "Node Data"
   (deftest dot-name-test
     (is (= (dot-name 'living-room) "living_room"))
     (is (= (dot-name 'foo!) "foo_"))
-    (is (= (dot-name '24) "24"))))
+    (is (= (dot-name '24) "24")))
+
+  (deftest dot-label-test
+    (is (= (dot-label '(garden (you are here)))
+           "(garden (you are here))"))
+    (is (= (dot-label '(Long text max of 30 chars goes here then dots))
+           "(Long text max of 30 chars goe..."))))
