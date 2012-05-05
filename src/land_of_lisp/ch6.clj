9de6b40969813c9b6f12f5a35f18ff092f741069
Jonathan Boston <boston@chgr.mc.vanderbilt.edu>
2012-05-05 13:48:39 -0500

    Finish game repl with user friendly messages

--- src/land_of_lisp/ch6.clj
+++ src/land_of_lisp/ch6.clj
@@ -1,6 +1,6 @@
 (ns land-of-lisp.ch6
-  (:require [land-of-lisp.core]
-            [land-of-lisp.ch5 :as ch5]))
+  (:require [land-of-lisp.core])
+  (:use [land-of-lisp.ch5]))
 
 (def allowed-commands '#{look walk pickup inventory})
 
@@ -12,6 +12,18 @@
                       (list 'quote (read-string param)))
         params (map quote-param (rest user-cmds))]
     (conj params func)))
+
+(defn pretty-print
+  [text-chars cap]
+  (let [[first-char & rest-chars] text-chars]
+    (cond
+      (nil? first-char) ""
+      (= first-char \space) (str first-char (pretty-print rest-chars cap))
+      (contains? #{\? \! \.} first-char)
+        (str first-char (pretty-print rest-chars true))
+      cap (str (clojure.string/capitalize (str first-char))
+               (pretty-print rest-chars false))
+      true (str first-char (pretty-print rest-chars false)))))
 
 ;non-functional UI
 (defn game-read
@@ -26,11 +38,14 @@
 
 (defn game-print
   [output]
-  output)
+  (println (pretty-print (seq (clojure.string/join " " output)) true)))
 
 (defn game-repl
   []
   (let [cmd (game-read)]
-    (when (not (= (first cmd) 'quit))
-            (game-print (game-eval cmd))
-            (game-repl))))
+    (if (= (first cmd) 'quit)
+      (game-print '(thanks for playing!))
+      (do
+        (game-print (game-eval cmd))
+        (flush) ;force message to print before returning to repl
+        (game-repl)))))
--- test/land_of_lisp/test/ch6.clj
+++ test/land_of_lisp/test/ch6.clj
@@ -6,3 +6,9 @@
   (is (= (convert-user-cmd "walk east") '(walk 'east)))
   (is (= (convert-user-cmd "run south yo") '(run 'south 'yo)))
   (is (= (convert-user-cmd "crawl") '(crawl))))
+
+(deftest pretty-print-test
+  (is (= (pretty-print (seq "just a test? try it!") true)
+         "Just a test? Try it!"))
+  (is (= (pretty-print (seq "i want a tablet, in other words an iPad.") true)
+         "I want a tablet, in other words an iPad.")))
