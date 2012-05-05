db0097b5601223e0c6ae7e20d1cf474b44d8503d
Jonathan Boston <boston@chgr.mc.vanderbilt.edu>
2012-05-05 17:38:54 -0500

    Can create dot info for nodes.
    Changed related functions to work with my data structures. The data
    structures are a bit different than the book's.

--- src/land_of_lisp/ch7.clj
+++ src/land_of_lisp/ch7.clj
@@ -4,12 +4,25 @@
 (def max-label-len 30)
 
 (defn dot-name
-  [raw-name]
-  (clojure.string/replace (str raw-name) #"[^\w]" "_"))
+  [name-key]
+  (clojure.string/upper-case
+    (clojure.string/replace (name name-key) #"[^\w]" "_")))
 
 (defn dot-label
-  [exp]
-  (let [s (str exp)]
-    (if (> (count s) max-label-len)
-      (str (subs s 0 max-label-len) "...")
-      s)))
+  [label-key label-desc]
+  (let [label-str (str (clojure.string/upper-case (name label-key))
+                       ": " label-desc)]
+        (if (> (count label-str) max-label-len)
+          (str (subs label-str 0 max-label-len) "...")
+          label-str)))
+
+(defn create-label
+  [node]
+  (let [[location desc] node
+        dot-location (dot-name location)]
+    (str dot-location "[label=\"" (dot-label dot-location desc) "\"];")))
+
+;how to map over map, with key and val as params to func
+(defn nodes->dot
+  [nodes]
+  (set (map create-label nodes)))
--- test/land_of_lisp/test/ch7.clj
+++ test/land_of_lisp/test/ch7.clj
@@ -4,12 +4,21 @@
 
 (testing "Node Data"
   (deftest dot-name-test
-    (is (= (dot-name 'living-room) "living_room"))
-    (is (= (dot-name 'foo!) "foo_"))
-    (is (= (dot-name '24) "24")))
+    (is (= (dot-name :living-room) "LIVING_ROOM"))
+    (is (= (dot-name :foo!) "FOO_"))
+    (is (= (dot-name :24) "24")))
 
   (deftest dot-label-test
-    (is (= (dot-label '(garden (you are here)))
-           "(garden (you are here))"))
-    (is (= (dot-label '(Long text max of 30 chars goes here then dots))
-           "(Long text max of 30 chars goe..."))))
+    (is (= (dot-label :garden '[you are here])
+           "GARDEN: [you are here]"))
+    (is (= (dot-label :lost '[Long text max of 30 chars goes here then dots])
+           "LOST: [Long text max of 30 cha...")))
+
+  (deftest nodes->dot-test
+    (let [nodes {:living-room '[you are in the living room.]
+                 :garden '[you are in a beautiful garden.]
+                 :attic '[you are in the attic.]}
+          expected #{"LIVING_ROOM[label=\"LIVING_ROOM: [you are in the l...\"];"
+                     "GARDEN[label=\"GARDEN: [you are in a beautifu...\"];"
+                     "ATTIC[label=\"ATTIC: [you are in the attic.]\"];"}]
+      (is (= #{} (clojure.set/difference (nodes->dot nodes) expected))))))
