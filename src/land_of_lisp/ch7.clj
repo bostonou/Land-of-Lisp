e3b02c82941eb70347c45cc2ad4edb3ab1831232
Jonathan Boston <boston@chgr.mc.vanderbilt.edu>
2012-05-07 22:36:17 -0500

    Creating edge dot info.

--- src/land_of_lisp/ch7.clj
+++ src/land_of_lisp/ch7.clj
@@ -16,13 +16,28 @@
           (str (subs label-str 0 max-label-len) "...")
           label-str)))
 
-(defn create-label
+(defn node-label
   [node]
   (let [[location desc] node
         dot-location (dot-name location)]
     (str dot-location "[label=\"" (dot-label dot-location desc) "\"];")))
 
-;how to map over map, with key and val as params to func
+(defn edge-label
+  [from edge-desc]
+  (let [[to & desc] edge-desc
+        dot-from (dot-name from)
+        dot-to (dot-name to)]
+    (str dot-from "[label=\"" (dot-label dot-to (vec desc)) "\"];")))
+
+(defn edge-labels
+  [edge]
+  (let [[from edge-desc] edge]
+    (map (partial edge-label from) edge-desc)))
+
 (defn nodes->dot
   [nodes]
-  (set (map create-label nodes)))
+  (set (map node-label nodes)))
+
+(defn edges->dot
+  [edges]
+  (set (flatten (map edge-labels edges))))
--- test/land_of_lisp/test/ch7.clj
+++ test/land_of_lisp/test/ch7.clj
@@ -2,7 +2,7 @@
   (:use [land-of-lisp.ch7])
   (:use [clojure.test]))
 
-(testing "Node Data"
+(testing "Dot format"
   (deftest dot-name-test
     (is (= (dot-name :living-room) "LIVING_ROOM"))
     (is (= (dot-name :foo!) "FOO_"))
@@ -12,8 +12,9 @@
     (is (= (dot-label :garden '[you are here])
            "GARDEN: [you are here]"))
     (is (= (dot-label :lost '[Long text max of 30 chars goes here then dots])
-           "LOST: [Long text max of 30 cha...")))
+           "LOST: [Long text max of 30 cha..."))))
 
+(testing "Node Data"
   (deftest nodes->dot-test
     (let [nodes {:living-room '[you are in the living room.]
                  :garden '[you are in a beautiful garden.]
@@ -22,3 +23,14 @@
                      "GARDEN[label=\"GARDEN: [you are in a beautifu...\"];"
                      "ATTIC[label=\"ATTIC: [you are in the attic.]\"];"}]
       (is (= #{} (clojure.set/difference (nodes->dot nodes) expected))))))
+
+(testing "Edge Data"
+  (deftest edges->dot-test
+    (let [edges {:living-room '[[:garden west door] [:attic upstairs ladder]]
+                 :garden '[[:living-room east door]]
+                 :attic '[[:living-room downstairs ladder]]}
+          expected #{"LIVING_ROOM[label=\"GARDEN: [west door]\"];"
+                     "LIVING_ROOM[label=\"ATTIC: [upstairs ladder]\"];"
+                     "GARDEN[label=\"LIVING_ROOM: [east door]\"];"
+                     "ATTIC[label=\"LIVING_ROOM: [downstairs ladde...\"];"}]
+      (is (= #{} (clojure.set/difference (edges->dot edges) expected))))))
