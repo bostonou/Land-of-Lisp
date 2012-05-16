3a8925b89ff6ba564d8207a1eb1a7a19be7f6791
Jonathan Boston <boston@chgr.mc.vanderbilt.edu>
2012-05-15 21:17:44 -0500

    Functions for converting nodes/edges to graph.

--- src/land_of_lisp/ch7.clj
+++ src/land_of_lisp/ch7.clj
@@ -1,7 +1,17 @@
 (ns land-of-lisp.ch7
-  (:require [land-of-lisp.core]))
+  (:require [land-of-lisp.core]
+            [clojure.java.io :as io]
+            [clojure.java.shell :as shell]))
 
 (def max-label-len 30)
+
+(def nodes {:living-room '[you are in the living room. a wizard is snoring loudly on the couch.]
+            :garden '[you are in a beautiful garden. there is a well in front of you.]
+            :attic '[you are in the attic. there is a giant welding torch in the corner.]})
+
+(def edges {:living-room '[[:garden west door] [:attic upstairs ladder]]
+            :garden '[[:living-room east door]]
+            :attic '[[:living-room downstairs ladder]]})
 
 (defn dot-name
   [name-key]
@@ -41,3 +51,22 @@
 (defn edges->dot
   [edges]
   (set (flatten (map edge-labels edges))))
+
+(defn graph->dot
+  [nodes edges]
+  (println "digraph{")
+  (println (clojure.string/join "\n" (nodes->dot nodes)))
+  (println (clojure.string/join "\n" (edges->dot edges)))
+  (println "}"))
+
+;non-functional
+(defn dot->png
+  [fname thunk]
+  (with-open [wrtr (io/writer fname)]
+    (.write wrtr (with-out-str (thunk))))
+  (shell/sh (str "dot -Tpng -O " fname)))
+
+(defn graph->png
+  [fname nodes edges]
+  (dot->png fname (partial #'graph->dot nodes edges)))
+
